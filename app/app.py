8aa0f5ace70ca17df677eee7a5962b439687a546
Meowster <meowster.sensei@gmail.com>
2020-09-03 03:54:19 +0000

    added more json dumps and created top level search

--- app/app.py
+++ app/app.py
@@ -1,16 +1,32 @@
 from flask import Flask
 from flask import jsonify
+from flask import request
 from flask_pymongo import PyMongo
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/meowster"
 mongo = PyMongo(app)
 
+# Get all from DB
 @app.route("/", methods = ["GET"])
 def home_page():
     dump = []
     #cursor = mongo.db.chest.find({"_id":{ "$eq": 10000 }},{"_id": 0,"element_defense": 1, "name": 1, "type":1})
     cursor = mongo.db.chest.find()
+    for indiv_ele in cursor:
+        dump.append(indiv_ele)
+    return jsonify(dump)
+
+# for debugging querying top level only
+@app.route("/search", methods = ["GET"])
+def top_search():
+    dump = []
+    key = request.args.get("key")
+    val = request.args.get("val")
+    if key in ["_id", "attack", "defense", "rarity", "gem_slot_size"]:
+        val = int(val)
+    cursor = mongo.db.chest.find({key: { "$eq" : val }})
     for indiv_ele in cursor:
         dump.append(indiv_ele)
     return jsonify(dump)
---
+++ usefulScripts.sh
@@ -0,0 +1,2 @@
+# improting json data into db
+mongoimport --jsonArray --db meowster --collection chest --file dump.json
