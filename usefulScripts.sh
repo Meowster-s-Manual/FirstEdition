00af92fe63b63af717b944927b46812c59b313df
MomentusCoffee <perplexedcoffee@gmail.com>
2020-09-09 04:32:17 +0000

    added vue

--- usefulScripts.sh
+++ usefulScripts.sh
@@ -1,2 +1,6 @@
 # improting json data into db
 mongoimport --jsonArray --db meowster --collection chest --file dump.json
+# vue ui setup for port 8081 pi4 ip address
+vue ui -H 192.168.1.111 -p 8081
+# serve our web page
+vue run serve
