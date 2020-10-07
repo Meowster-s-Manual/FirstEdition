29c0a175c409a5857532bdd46caf6a71aba15c2b
MomentusCoffee <MomentusCoffee@github.com>
2020-10-06 22:11:29 -0700

    organized fs + mongodb import

---
+++ test/import.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+mongoimport --jsonArray --db meowster --collection chest --username meowster --password 3L3T3 --authenticationDatabase admin --file dump.json
--- usefulScripts.sh
+++ usefulScripts.sh
@@ -7,7 +7,7 @@ vue run serve
 #enter mongodb in the container
 mongo --username meowster --password 3L3T3
 #copying dump.json to docker
-docker cp dump.json 4190c854ba0f:dump.json
+docker cp dump.json mongodb:dump.json
 #mongo import inside the container
 mongoimport --jsonArray --db meowster --collection chest --username meowster --password 3L3T3 --authenticationDatabase admin --file dump.json
 
