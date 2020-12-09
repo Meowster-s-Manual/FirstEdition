469d8f1fcd4fa153571a65abc29981f88e8e82a4
MomentusCoffee <perplexedcoffee@gmail.com>
2020-12-08 22:25:36 -0800

    added bootstrap

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,8 +1,21 @@
 <template>
   <div class="hello">
-    <p>
-      WHAT ARE YOU DOING ANDREW!!!!!
-    </p>
+    <b-card
+      title="WHAT ARE YOU DOING ANDREW!!!!!!!!!!!!!!!!!!"
+      img-src="https://media-exp1.licdn.com/dms/image/C4D03AQHDlAmFeET8EQ/profile-displayphoto-shrink_400_400/0/1605769341718?e=1613001600&v=beta&t=Kq7OCBVKw3SsYCcrhoZ6OGxZbHK6noil-_DetW7duu4"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem; text-align: center; margin: 0 auto;"
+      class="mb-2"
+    >
+      <b-card-text>
+        Some quick example text to build on the card title and make up the bulk of the card's content.
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Go somewhere</b-button>
+    </b-card>
+
   </div>
 </template>
 
--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import Index from './Index.vue'
 import router from './router.js'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(Index),
