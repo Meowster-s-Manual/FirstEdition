0e4537c803c26054c8f3cbec8b3c2c55da7b9594
andrew-suh <andrew.suh.94@gmail.com>
2020-12-16 23:45:53 -0600

    Added scrollable component at the right of the home screen

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -6,7 +6,7 @@
       img-alt="Image"
       img-top
       tag="article"
-      style="max-width: 20rem; text-align: center; margin: 0 auto;"
+      style="text-align: center;"
       class="mb-2"
     >
       <b-card-text>
@@ -15,15 +15,57 @@
 
       <b-button href="#" variant="primary">Go somewhere</b-button>
     </b-card>
-
+    <b-card no-body>
+      <b-card-body
+        id="nav-scroller"
+        ref="content"
+        style="max-width: 30rem;right: 10px; height:1000px; overflow-y:scroll;"
+      >
+        <p>{{ text }}</p>
+        <h4 id="fat">@fat</h4>
+        <p v-for="i in 3" :key="i">{{ text }}</p>
+        <h4 id="mdo">@mdo</h4>
+        <p v-for="i in 3" :key="i">{{ text }}</p>
+        <h4 id="one">one</h4>
+        <p v-for="i in 2" :key="i">{{ text }}</p>
+        <h4 id="two">two</h4>
+        <p>{{ text }}</p>
+        <h4 id="three">three</h4>
+        <p v-for="i in 2" :key="i">{{ text }}</p>
+        <h4 id="pi0">@pi0</h4>
+        <p v-for="i in 3" :key="i">{{ text }}</p>
+      </b-card-body>
+    </b-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  methods: {
+    // Convenience method to scroll a heading into view.
+    // Not required for scrollspy to work
+    scrollIntoView(event) {
+      event.preventDefault()
+      const href = event.target.getAttribute('href')
+      const el = href ? document.querySelector(href) : null
+      if (el) {
+        this.$refs.content.scrollTop = el.offsetTop
+      }
+    }
+  },
+  data() {
+    return {
+      text: `
+        Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
+        tempor. Laborum consequat non elit enim exercitation cillum aliqua
+        consequat id aliqua. Esse ex consectetur mollit voluptate est in duis
+        laboris ad sit ipsum anim Lorem. Incididunt veniam velit elit elit veniam
+        Lorem aliqua quis ullamco deserunt sit enim elit aliqua esse irure. Laborum
+        nisi sit est tempor laborum mollit labore officia laborum excepteur
+        commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
+        ipsum commodo tempor sunt in proident.
+      `
+    }
   }
 }
 </script>
@@ -43,5 +85,11 @@ li {
 }
 a {
   color: #42b983;
+}
+
+.hello {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
 }
 </style>
