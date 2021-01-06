eb4515bd833eda3129b0a8414449454153a6a3c9
Kevin Yook <yook00627@gmail.com>
2021-01-05 20:22:37 -0800

    set up data to show up

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -21,26 +21,31 @@
         ref="content"
         style="max-width: 30rem;right: 10px; height:1000px; overflow-y:scroll;"
       >
-        <p>{{ text }}</p>
-        <h4 id="fat">@fat</h4>
-        <p v-for="i in 3" :key="i">{{ text }}</p>
-        <h4 id="mdo">@mdo</h4>
-        <p v-for="i in 3" :key="i">{{ text }}</p>
-        <h4 id="one">one</h4>
-        <p v-for="i in 2" :key="i">{{ text }}</p>
-        <h4 id="two">two</h4>
-        <p>{{ text }}</p>
-        <h4 id="three">three</h4>
-        <p v-for="i in 2" :key="i">{{ text }}</p>
-        <h4 id="pi0">@pi0</h4>
-        <p v-for="i in 3" :key="i">{{ text }}</p>
+        <h4 id="item_list">Items</h4>
+        <p v-for="data in dbData" :key="'item_list'+data._id">{{ data }}</p>
       </b-card-body>
     </b-card>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
+  data() {
+    return {
+      dbData: {},
+    }
+  },
+
+  created() {
+    axios.get('http://localhost:8084').then(dump =>{
+      this.dbData = dump.data;
+      }).catch(err =>{
+        console.log(err);
+    });
+  },
+
   methods: {
     // Convenience method to scroll a heading into view.
     // Not required for scrollspy to work
@@ -53,20 +58,6 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      text: `
-        Quis magna Lorem anim amet ipsum do mollit sit cillum voluptate ex nulla
-        tempor. Laborum consequat non elit enim exercitation cillum aliqua
-        consequat id aliqua. Esse ex consectetur mollit voluptate est in duis
-        laboris ad sit ipsum anim Lorem. Incididunt veniam velit elit elit veniam
-        Lorem aliqua quis ullamco deserunt sit enim elit aliqua esse irure. Laborum
-        nisi sit est tempor laborum mollit labore officia laborum excepteur
-        commodo non commodo dolor excepteur commodo. Ipsum fugiat ex est consectetur
-        ipsum commodo tempor sunt in proident.
-      `
-    }
-  }
 }
 </script>
 
